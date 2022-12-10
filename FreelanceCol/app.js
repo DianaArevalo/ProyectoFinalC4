660bee1a63414bfde3395dc4706f3c3a155711f5
eafg1986 <edwin.fuy.mt@usa.edu.co>
2022-12-10 18:51:52 -0500

    cargando company

--- FreelanceCol/app.js
+++ FreelanceCol/app.js
@@ -1,12 +1,27 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import companyRouter from "./routes/companyRouter.js";
 
 const app = express();
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8081;
 
-app.listen(port, ()=> {
-    console.log("El servidor se esta ejecutando.");
-})
+app.listen(port, () => {
+  console.log("El servidor se esta ejecutando.");
+});
+
+mongoose.connect(
+  "mongodb+srv://FreelanceCol:grupo5@freelancecol.pzdp53s.mongodb.net/Freelance-Col?retryWrites=true&w=majority",
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Conectado a DB.");
+    }
+  }
+);
 
 //Midleware
-app.use(express.json())
+app.use(express.json());
+app.use("/company", companyRouter);
+app.use("/company/all", companyRouter);
