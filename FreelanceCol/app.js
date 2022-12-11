8c03e31885a7d3c5bc69f3d3d235504f6f5c1325
TeoProgramming <programingtop@gmail.com>
2022-12-10 22:18:17 -0500

    Proyecto conectado a base de datos y funcionando

--- FreelanceCol/app.js
+++ FreelanceCol/app.js
@@ -10,6 +10,16 @@ app.listen(port, ()=> {
     console.log("El servidor se esta ejecutando.");
 })
 
+mongoose.connect("mongodb+srv://FreelanceCol:grupo5@freelancecol.pzdp53s.mongodb.net/?retryWrites=true&w=majority",(err) =>{
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("La base de datos se encuentra conectada.");
+    }
+
+
+})
+
 //Midleware
 app.use(express.json())
 app.use("/lesson", lessonRouter)
