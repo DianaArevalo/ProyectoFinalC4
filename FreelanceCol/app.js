104296babdf57b0e643a169f8d4781baef302766
DianaArevalo <morec9834@gmail.com>
2022-12-10 23:21:44 -0500

    Ultimos cambios

--- FreelanceCol/app.js
+++ FreelanceCol/app.js
@@ -1,12 +1,29 @@
 import express from "express"
+import clientRouter from "./routes/clientRouter.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8082
 
 app.listen(port, ()=> {
     console.log("El servidor se esta ejecutando.");
 })
 
+mongoose.connect("mongodb+srv://FreelanceCol:grupo5@freelancecol.pzdp53s.mongodb.net/Freelance-Col?retryWrites=true&w=majority", (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+    console.log("la base de datos se encuentra conectada.");
+    }
+})
+
 //Midleware
 app.use(express.json())
+app.use("/client", clientRouter)
+app.use("/client/profile", clientRouter)
+app.use("/client/reservations", clientRouter)
+app.use("/client/searchD", clientRouter)
+app.use("/client/searchC", clientRouter)
+app.use("/client/update", clientRouter)
+app.use("/client/deleteUser", clientRouter)
--- FreelanceCol/routes/clientRouter.js
+++ FreelanceCol/routes/clientRouter.js
@@ -1,22 +1,41 @@
 import express from "express"
+import { createClient, deleteClient, readClient, updateClient } from "../controller/clientController.js"
 
 const clientRouter = express.Router()
 
 //CRUD
 //CREAR: POST
-clientRouter.post("/nuevo", (req, res) => {
-
+clientRouter.post("/", (req, res) => {
+    createClient(req, res)
 })
 
 //LEER: GET
-clientRouter.get("/nuevo", (req, res) => {
+clientRouter.get("/:profile", (req, res) => {
+    readClient(res)
+
+})
+
+//clientRouter.post("/reservations", (req, res) => {
+    //createClient(req, res)
+
+//})
+clientRouter.get("/searchD", (req, res) => {
+    readClient( res)
+
+})
+clientRouter.get("/searchC", (req, res) => {
+    readClient( res)
 
 })
 //ACTUALIZAR: PUT/ PATCH
-clientRouter.patch("/nuevo", (req, res) => {
+clientRouter.patch("/", (req, res) => {
+    updateClient( res)
 
 })
 //ELIMINAR: DELETE
-clientRouter.delete("/nuevo", (req, res) => {
+clientRouter.delete("/", (req, res) => {
+    deleteClient(res)
 
 })
+
+export default clientRouter
